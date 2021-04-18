c65a70d7f096fee9998523fce3a176ad0a1c8388
evgenijerm <erm.evgeniy18@gmail.com>
2021-04-18 12:12:52 +0300

    CI17

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -49,5 +49,18 @@ class CallbackTest {
         String text = driver.findElement(By.className("paragraph")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
+
+    //    @Test
+//    void shouldTestV2() {
+//        driver.get("http://localhost:9999");
+//        WebElement form = driver.findElement(By.cssSelector("[enctype=application/x-www-form-urlencoded]"));
+//        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
+//        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
+//        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+//        form.findElement(By.cssSelector("[data-test-id=submit]")).click();
+//        String text = driver.findElement(By.className("alert-success")).getText();
+//        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+//     }
+
 }
 
